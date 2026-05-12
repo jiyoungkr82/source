fb9a9334a59ba0d1d8e57193ba46b7210eca9545
jiyoung <jiyoung.kr82@gmail.com>
2026-05-12 17:58:56 +0900

    파이썬 문법 기초

--- pythonSource/basic/hello.py
+++ pythonSource/basic/hello.py
@@ -1,1 +1,15 @@
-print("Hello")
+# print("Hello")
+
+# 줄 단위 실행 => 특정 행에서 실행 오류가 나는 경우 프로그램은 멈추게 됨
+# 파이썬 자료형
+# 정수형, 문자형, 불린형, 리스트형, 튜플, 딕셔너리, set
+
+# 변수 : 프로그램 안에서 값을 담아놓기 위한 공간(이름 사용)
+a = 123
+print(a)
+
+# \n == enter
+multiline = "Life is too short\nYou need python"
+print(multiline)
+
+
